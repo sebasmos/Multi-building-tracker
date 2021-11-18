5ba413010dc66b3ef0d3198a4f9c35b4274b2bb7
sebasmos <ulsordonez@unicauca.edu.co>
2021-11-18 19:30:19 +0100

    update unet and unpatch

--- codes/tester_unet.py
+++ codes/tester_unet.py
@@ -21,9 +21,9 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 # Clean graphics memory
-#import gc
-#gc.collect()
-#torch.cuda.empty_cache()
+import gc
+gc.collect()
+torch.cuda.empty_cache()
 
 # Set device
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
@@ -34,7 +34,9 @@ num_classes = 1
 learning_rate = 1e-3
 batch_size = 8
 num_epochs = 2
+
 chip_dimension = 256
+#chip_dimension = 512
 
 train_dir = Path('../../DATASET/dataset_pruebas/train')
 test_dir = Path('../../DATASET/dataset_pruebas/validation')
@@ -51,10 +53,10 @@ std = [0.229, 0.224, 0.225]
 A.Normalize(mean=mean,std=std)
 A.Rotate(limit=(-360, 360), interpolation=4, border_mode=4,p=1),
 
-chip_dimension = 256
 
 transform = A.Compose(
     [
+        A.Resize(height=100, width=100),
         A.PadIfNeeded(min_height=chip_dimension,min_width=chip_dimension,value=0,p=1),
         A.RandomRotate90(p=1.0),
         ToTensorV2()
@@ -64,12 +66,12 @@ train_set   = Dataloader_trdp(root_dir=root_dir,csv_file=csv_file,chip_dimension
 
 testing_set = Dataloader_trdp(root_dir=root_dir,csv_file=csv_file_test,chip_dimension=chip_dimension,transform=transform)
 
-train_set, test_set = torch.utils.data.random_split(train_set, [1000,504])
+train_set, val_set = torch.utils.data.random_split(train_set, [round(0.7*len(train_set)),round(0.3*len(train_set))])
 
 
 train_loader = DataLoader(dataset = train_set, batch_size=batch_size, shuffle = True)
-test_loader = DataLoader(dataset = test_set, batch_size=batch_size, shuffle = True)
-
+val_loader = DataLoader(dataset = val_set, batch_size=batch_size, shuffle = False)
+test_loader = DataLoader(dataset = testing_set, batch_size=batch_size, shuffle = False)
 import torch
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
@@ -89,11 +91,8 @@ from utils import (
 
 # Hyperparameters etc.
 LEARNING_RATE = 1e-4
-NUM_WORKERS = 2
-IMAGE_HEIGHT = 160  # 1280 originally
-IMAGE_WIDTH = 240  # 1918 originally
 PIN_MEMORY = True
-LOAD_MODEL = False
+LOAD_MODEL = True
 
 def train_fn(loader, model, optimizer, loss_fn, scaler):
     
@@ -104,8 +103,7 @@ def train_fn(loader, model, optimizer, loss_fn, scaler):
         images = data["raster_diff"].float()
         targets = data["mask_diff"].float()#type(torch.LongTensor)
        # images, mask = images.to(device), mask.to(device).unsqueeze(1)
-        
-        
+      
         
         images = images.to(device=DEVICE)
         #targets = targets.float().unsqueeze(1).to(device=DEVICE) #
@@ -140,8 +138,21 @@ scaler = torch.cuda.amp.GradScaler()
 
 
 for epoch in range(num_epochs):
-        train_fn(train_loader, model, optimizer, loss_fn, scaler)
-        check_accuracy(test_loader, model, device=DEVICE)
+        #train_fn(train_loader, model, optimizer, loss_fn, scaler)
+
+
+        #check_accuracy(train_loader, model, device=DEVICE)
+        
+        
+        # print some examples to a folder
+        save_predictions_as_imgs(val_loader, model, folder="saved_images/", device=DEVICE)
+        
+        # save model
+        checkpoint = {
+            "state_dict": model.state_dict(),
+            "optimizer":optimizer.state_dict(),
+        }
+        save_checkpoint(checkpoint)
 
         '''
         # save model
@@ -156,8 +167,9 @@ for epoch in range(num_epochs):
 
         # print some examples to a folder
         save_predictions_as_imgs(
-            test_loader, model, folder="saved_images/", device=DEVICE
+            val_loader, model, folder="saved_images/", device=DEVICE
         )
         '''
+
 
 
--- codes/utils.py
+++ codes/utils.py
@@ -1,7 +1,6 @@
 import torch
 import torchvision
-#from dataset import CarvanaDataset
-#from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader
 
 def save_checkpoint(state, filename="my_checkpoint.pth.tar"):
     print("=> Saving checkpoint")
@@ -10,61 +9,21 @@ def save_checkpoint(state, filename="my_checkpoint.pth.tar"):
 def load_checkpoint(checkpoint, model):
     print("=> Loading checkpoint")
     model.load_state_dict(checkpoint["state_dict"])
-'''
-def get_loaders(
-    train_dir,
-    train_maskdir,
-    val_dir,
-    val_maskdir,
-    batch_size,
-    train_transform,
-    val_transform,
-    num_workers=4,
-    pin_memory=True,
-):
-    train_ds = CarvanaDataset(
-        image_dir=train_dir,
-        mask_dir=train_maskdir,
-        transform=train_transform,
-    )
 
-    train_loader = DataLoader(
-        train_ds,
-        batch_size=batch_size,
-        num_workers=num_workers,
-        pin_memory=pin_memory,
-        shuffle=True,
-    )
-
-    val_ds = CarvanaDataset(
-        image_dir=val_dir,
-        mask_dir=val_maskdir,
-        transform=val_transform,
-    )
-
-    val_loader = DataLoader(
-        val_ds,
-        batch_size=batch_size,
-        num_workers=num_workers,
-        pin_memory=pin_memory,
-        shuffle=False,
-    )
-
-    return train_loader, val_loader
-'''
 def check_accuracy(loader, model, device="cuda"):
     num_correct = 0
     num_pixels = 0
     dice_score = 0
     model.eval()
+    print("ENTROOOO")
     with torch.no_grad():
-            for data in test_loader:
+            for idx, data in enumerate(loader):
                 x = data["raster_diff"].float()
                 y = data["mask_diff"].float()
                 x = x.to(device)
-                y = y.to(device).unsqueeze(1)
+                y = y.to(device).squeeze()
                 print(f"x.shape : {x.shape}, y.shape : {y.shape}")
-                preds = torch.sigmoid(model(x))
+                preds = torch.sigmoid(model(x).squeeze())
                 preds = (preds > 0.5).float()
                 num_correct += (preds == y).sum()
                 num_pixels += torch.numel(preds)
@@ -82,15 +41,20 @@ def save_predictions_as_imgs(
     loader, model, folder="saved_images/", device="cuda"
 ):
     model.eval()
-    for idx, (x, y) in enumerate(loader):
-        x = x.to(device=device)
+    
+    for idx, data in enumerate(loader):
+        x = data["raster_diff"].float()
+        y = data["mask_diff"].float()
+        x = x.to(device)
+        y = y.to(device).squeeze()
         with torch.no_grad():
-            preds = torch.sigmoid(model(x))
+            preds = torch.sigmoid(model(x).squeeze())
             preds = (preds > 0.5).float()
+            print(preds.shape)
+    
         torchvision.utils.save_image(
-            preds, f"{folder}/pred_{idx}.png"
+            preds, f"{folder}/pred_{idx}.tiff"
         )
-        torchvision.utils.save_image(y.unsqueeze(1), f"{folder}{idx}.png")
+        torchvision.utils.save_image(y.squeeze(), f"{folder}{idx}.png")
 
     model.train()
-
